ac2d0ee12ff3d04c3ebc1b6f14e276e6475c0097
Muwu Hou <mhou@linkedin.biz>
2020-08-23 17:20:37 -0700

    qt helloworld update

--- helloworld/hello.cpp
+++ helloworld/hello.cpp
@@ -1,7 +1,36 @@
 #include <QTextStream>
 
+
+QTextStream output(stdout);
+
+QTextStream& operator<<(QTextStream& out, const QStringList sl)
+{
+    out << '[';
+    bool first = true;
+    for (const auto& e: sl) {
+        if (!first) {
+            out << ", ";
+        }
+        out << '"' << e << '"';
+        first = false;
+    }
+    out << "] ";
+    return out;
+}
+
+void test1()
+{
+    QString s1 = "hello world";
+    output << "s1: " << s1 << Qt::endl;
+    output << "s1.size: " << s1.size() << Qt::endl;
+    QStringList tokens = s1.split(" ");
+    output << "tokens: " << tokens << Qt::endl;
+}
+
+
 int main(int argc, char* argv[])
 {
-    QTextStream(stdout) << "hello world!" << Qt::endl;
+    output << "hello world!" << Qt::endl;
+    test1();
     return 0;
 }
